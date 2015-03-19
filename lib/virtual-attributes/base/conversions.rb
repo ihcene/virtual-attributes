34247979cc9d056ecd6ff3d2e32811ba840d631b
Ihcène Medjber <idelways@gmail.com>
2015-03-19 14:31:51 +0100

    Add more convertible types

--- lib/virtual-attributes/base/conversions.rb
+++ lib/virtual-attributes/base/conversions.rb
@@ -7,8 +7,14 @@ class VirtualAttributes::Base
         case val
         when Hash
           new(val)
+        when String
+          val.blank? ? new({}) : raise(ArgumentError('String is not convertible to #{self.name}'))
         when self
           val
+        when NilClass
+          new({})
+        when OpenStruct
+          new val.to_h
         else
           raise ArgumentError.new("#{val.class.name} is not convertible to #{self.name}")
         end
